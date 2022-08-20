4379716751509b759964627647baa2ca6f155466
Subho Ghosh <subho.ghosh@outlook.com>
2022-08-20 10:30:12 +0530

    Sync

--- docker/run.sh
+++ docker/run.sh
@@ -65,8 +65,6 @@ sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/hdfs-site.xml.t
 
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
-ls -l /usr/local/hadoop/etc/hadoop
-
 echo SERVER=$server CLUSTER_NAME=$CLUSTER_NAME NNODE1_IP=$NNODE1_IP NNODE2_IP=$NNODE2_IP JNODES=$JNODES ZK_IPS=$ZK_IPS
 
 if [[ $server = "format" ]]; then
